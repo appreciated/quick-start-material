256c3c54bd1406950b80124eab7dacd0324110a6
appreciated <goebeljohannes@gmx.net>
2017-04-15 13:59:36 +0200

    js fixes

--- src/main/webapp/VAADIN/component/desktop.js
+++ src/main/webapp/VAADIN/component/desktop.js
@@ -1,22 +1,28 @@
-var element = document.getElementById('contentPanel'); 
-                var childElement = element.getElementsByClassName('v-panel-content').item(0); 
-                console.log('test'); 
-                childElement.addEventListener('scroll', function () { 
-                    if (document.getElementById('navigation-bar')) { 
-                        if (childElement.scrollTop > 0) { 
-                            if (!document.getElementById('navigation-bar').classList.contains('floating-navigation-bar')) { 
-                                document.getElementById('navigation-bar').classList.add('floating-navigation-bar') 
-                                console.log('added'); 
-                            } 
-                        } 
-                        else { 
-                            if (document.getElementById('navigation-bar').classList.contains('floating-navigation-bar')) { 
-                                document.getElementById('navigation-bar').classList.remove('floating-navigation-bar'); 
-                                console.log('removed'); 
-                            } 
-                        } 
-                    } 
-                    else { 
-                        console.log('no element'); 
-                    } 
-                }););
+window.onload = function () {
+    var body = document.body;
+    body.addEventListener('DOMNodeInserted', OnNodeInserted, false);
+
+    function OnNodeInserted(event) {
+        var element = document.getElementById('contentPanel');
+        var childElement = element.getElementsByClassName('v-panel-content').item(0);
+        childElement.addEventListener('scroll', function () {
+            if (document.getElementById('navigation-bar')) {
+                if (childElement.scrollTop > 0) {
+                    if (!document.getElementById('navigation-bar').classList.contains('floating-navigation-bar')) {
+                        document.getElementById('navigation-bar').classList.add('floating-navigation-bar')
+                        console.log('added');
+                    }
+                }
+                else {
+                    if (document.getElementById('navigation-bar').classList.contains('floating-navigation-bar')) {
+                        document.getElementById('navigation-bar').classList.remove('floating-navigation-bar');
+                        console.log('removed');
+                    }
+                }
+            }
+            else {
+                console.log('no element');
+            }
+        });
+    }
+}
--- src/main/webapp/VAADIN/component/mobile.js
+++ src/main/webapp/VAADIN/component/mobile.js
@@ -1,56 +1,62 @@
-var menuButtonId = 'menu-button';
-var menuId = 'menu-wrapper';
-document.getElementById(menuButtonId).onclick = function () {
-document.getElementById(menuId).classList.toggle('menu-show');
-};);
-var elements = document.getElementsByClassName('mobile-tab-wrapper');
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener(\click\, function () {
-        document.getElementById(menuId).classList.toggle('menu-show');
-    });
-});
-/**
- * Close/open the menu when swiping left/right
- */
-var xDown = null;
-var yDown = null;
+window.onload = function () {
+    var body = document.body;
+    body.addEventListener('DOMNodeInserted', OnNodeInserted, false);
 
-function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
-}
+    function OnNodeInserted(event) {
+        var menuButtonId = 'menu-button';
+        var menuId = 'menu-wrapper';
+        document.getElementById(menuButtonId).onclick = function () {
+            document.getElementById(menuId).classList.toggle('menu-show');
+        };
+        var elements = document.getElementsByClassName('mobile-tab-wrapper');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(\click\, function () {
+                document.getElementById(menuId).classList.toggle('menu-show');
+            });
+        }
+        /**
+         * Close/open the menu when swiping left/right
+         */
+        var xDown = null;
+        var yDown = null;
 
-function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-        return;
+        function handleTouchStart(evt) {
+            xDown = evt.touches[0].clientX;
+            yDown = evt.touches[0].clientY;
+        }
+
+        function handleTouchMove(evt) {
+            if (!xDown || !yDown) {
+                return;
+            }
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+            if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
+                console.log(Math.abs(xDiff));
+                if (xDiff > 0 && Math.abs(xDiff) >= 10) {
+                    // left swipe
+                    document.getElementById(menuId).classList.remove('menu-show');
+                }
+                else if (Math.abs(xDiff) >= 10) {
+                    // right swipe
+                    document.getElementById(menuId).classList.add('menu-show');
+                }
+            }
+            else {
+                if (yDiff > 0) {
+                    // console.log(\up swipe\);
+                }
+                else {
+                    // console.log(\down swipe\);
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        }
+        document.addEventListener('touchstart', handleTouchStart, false);
+        document.addEventListener('touchmove', handleTouchMove, false);
     }
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-    if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
-        console.log(Math.abs(xDiff));
-        if (xDiff > 0 && Math.abs(xDiff) >= 10) {
-            // left swipe
-            document.getElementById(menuId).classList.remove('menu-show');
-        }
-        else if (Math.abs(xDiff) >= 10) {
-            // right swipe
-            document.getElementById(menuId).classList.add('menu-show');
-        }
-    }
-    else {
-        if (yDiff > 0) {
-            // console.log(\up swipe\);
-        }
-        else {
-            // console.log(\down swipe\);
-        }
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;
 }
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove'
-        ', handleTouchMove, false););
