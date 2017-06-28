7e571743c02b722391d94a2ea2bdbdac088831dc
appreciated <goebeljohannes@gmx.net>
2017-06-28 23:49:42 +0200

    - bugfix

--- src/main/java/com/github/appreciated/quickstart/material/components/MaterialNavigationContainerView.java
+++ src/main/java/com/github/appreciated/quickstart/material/components/MaterialNavigationContainerView.java
@@ -1,9 +1,17 @@
 package com.github.appreciated.quickstart.material.components;
 
 
+import com.github.appreciated.quickstart.base.navigation.interfaces.base.ContainerSubpage;
+
 /**
  * Created by appreciated on 11.12.2016.
  */
 public class MaterialNavigationContainerView extends MaterialNavigationContainerDesign {
 
+    public MaterialNavigationContainerView(ContainerSubpage page) {
+        this.addComponent(page);
+        if (page.hasPadding()) {
+            addStyleName("container-padding");
+        }
+    }
 }
